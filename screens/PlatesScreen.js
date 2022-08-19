58f3d841e4fd5e14e7a6dc85b99e1912627c8e1b
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-08-19 17:36:01 -0600

    plate names displaying on page

--- screens/PlatesScreen.js
+++ screens/PlatesScreen.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { ScrollView, StyleSheet, View, ImageBackground, SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import { ScrollView, StyleSheet, View, ImageBackground, SafeAreaView, Text, Image, TouchableOpacity } from 'react-native';
 import { DefaultTheme } from 'react-native-paper';
+import * as BlankPlates from '../PlateData.json';
+
 //image source for plates = ./assets/{country}/{toLower(name)}.jpg
 function PlatesScreen() {
+  let BlankPlateData = BlankPlates.PlateData;
+
+  console.log(BlankPlateData);
+
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={styles.scrollView}>
-      <Text>License Plates</Text>
+      <View>
+        {BlankPlateData.map((plate) => {
+          return (
+            <View key={plate.id}>
+              <Image source={'../assets/USA/Colorado.jpg'} />
+              <Text>{plate.name}</Text>
+            </View>
+          );
+        })}
+      </View>
     </ScrollView>
   );
 };
