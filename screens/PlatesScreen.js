915f59df1b64e407e8069c23ae2d068133221317
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-08-19 16:50:21 -0600

    adding start game data

--- screens/PlatesScreen.js
+++ screens/PlatesScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ScrollView, StyleSheet, View, ImageBackground, SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import { DefaultTheme } from 'react-native-paper';
-
+//image source for plates = ./assets/{country}/{toLower(name)}.jpg
 function PlatesScreen() {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={styles.scrollView}>
