cc4e205e071781ad4c1ce211bdfbbe32141b6ebf
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-08-19 18:46:32 -0600

    got image to show

--- screens/PlatesScreen.js
+++ screens/PlatesScreen.js
@@ -6,16 +6,16 @@ import * as BlankPlates from '../PlateData.json';
 //image source for plates = ./assets/{country}/{toLower(name)}.jpg
 function PlatesScreen() {
   let BlankPlateData = BlankPlates.PlateData;
-
-  console.log(BlankPlateData);
+  let pictureTest = require('../assets/USA/alabama.jpg');
 
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={styles.scrollView}>
+      <Image style={styles.image} source={pictureTest} />
       <View>
         {BlankPlateData.map((plate) => {
           return (
             <View key={plate.id}>
-              <Image source={'../assets/USA/Colorado.jpg'} />
+              <Image source={'./assets/USA/colorado.jpg'} />
               <Text>{plate.name}</Text>
             </View>
           );
@@ -26,7 +26,9 @@ function PlatesScreen() {
 };
 
 const styles = StyleSheet.create({
-
+  image: {
+    marginTop: 40
+  }
 })
 
 export default PlatesScreen;
