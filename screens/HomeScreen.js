b14b04e4d6ee383b97736a6db1c0d6ce6e5eab8a
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-08-25 13:37:04 -0600

    confirm mark not found functionality

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   headerText: {
-    fontSize: 40,
+    fontSize: 30,
     fontWeight: 'bold',
   },
   buttonText: {
--- screens/PlatesScreen.js
+++ screens/PlatesScreen.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { ScrollView, StyleSheet, View, ImageBackground, SafeAreaView, Text, Image, TouchableOpacity, RefreshControl } from 'react-native';
+import { ScrollView, StyleSheet, View, ImageBackground, SafeAreaView, Text, Image, TouchableOpacity, RefreshControl, Alert } from 'react-native';
 import { Button, DefaultTheme } from 'react-native-paper';
 import BlankPlates from '../PlateData.json';
+import { Colors } from '../assets/colors';
 
 //image source for plates = ./assets/{country}/{toLower(name)}.jpg
 function PlatesScreen() {
@@ -9,45 +10,93 @@ function PlatesScreen() {
   const [refresh, setRefresh] = useState(0);
 
   const foundPlate = (plate) => {
-    let gameArr = gameState;
-    const plateIdx = gameArr.findIndex(p => p.id === plate.id);
-    gameArr[plateIdx].found = true;
-    setGameState(gameArr);
-    setRefresh(refresh + 1);
+    if (plate.found) {
+      Alert.alert(
+        "Do you want to mark " + plate.name + " as not found?",
+        "",
+        [
+          {
+            text: "No",
+            onPress: () => setRefresh(refresh + 1),
+            style: "cancel"
+          },
+          {
+            text: "Yes", onPress: () => {
+              let gameArr = gameState;
+              const plateIdx = gameArr.findIndex(p => p.id === plate.id);
+              gameArr[plateIdx].found = false;
+              setGameState(gameArr);
+              setRefresh(refresh + 1);
+            }
+          }
+        ]
+      );
+    } else {
+      let gameArr = gameState;
+      const plateIdx = gameArr.findIndex(p => p.id === plate.id);
+      gameArr[plateIdx].found = true;
+      setGameState(gameArr);
+      setRefresh(refresh + 1);
+    }
   }
 
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={styles.scrollView}>
+    <View>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }} style={styles.scrollView}>
 
-      <View style={styles.main}>
-        <Text style={{ marginTop: 40, padding: 5, fontSize: '20px', fontWeight: 'bold' }}>Tap the plates below to mark them as found</Text>
-        {gameState.map((plate) => {
-          return (
-            <View key={plate.id}>
-              <TouchableOpacity onPress={() => { foundPlate(plate) }} style={styles.button}>
+        <View style={styles.main}>
+          <Text style={{ marginTop: 40, padding: 5, fontSize: '20px', fontWeight: 'bold' }}>Tap the plates below to mark them as found</Text>
+          {gameState.map((plate) => {
+            return (
+              <View key={plate.id}>
+                <TouchableOpacity onPress={() => { foundPlate(plate) }} style={styles.plateButton}>
 
-                <Image source={require('../assets/USA/CO.jpg')} style={plate.found ? styles.image : null} />
-                <Text style={styles.text}>{plate.name}</Text>
-              </TouchableOpacity>
-            </View>
-          );
-        })}
+                  <Image source={require('../assets/USA/CO.jpg')} style={plate.found ? styles.image : null} />
+                  <Text style={styles.text}>{plate.name}</Text>
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+
+        </View>
+      </ScrollView>
+      <View style={styles.buttonBar}>
+        <TouchableOpacity style={[styles.gameButton, { backgroundColor: Colors.signYellow }]} onPress>
+          <Text style={{ fontSize: '30px' }}>Restart</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.gameButton, { backgroundColor: Colors.signRed }]} onPress>
+          <Text style={{ fontSize: '30px' }}>Finish</Text>
+        </TouchableOpacity>
       </View>
-    </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
+  buttonBar: {
+    flex: 1,
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 30
+  },
+  gameButton: {
+    flex: 3,
+    height: 75,
+    margin: (0, 5, 0, 5),
     alignItems: 'center',
-    borderWidth: '1',
+    justifyContent: 'center',
+    borderRadius: 10,
   },
   image: {
     opacity: 0.5
   },
   main: {
     justifyContent: 'center'
+  },
+  plateButton: {
+    alignItems: 'center',
+    borderWidth: '1',
   },
   text: {
     fontSize: '50px',
